e325a7066309ebbff7a5c521eef851fcf04f043a
neargye <neargye@gmail.com>
2019-11-18 13:35:45 +0500

    fix macro check support

--- include/magic_enum.hpp
+++ include/magic_enum.hpp
@@ -45,6 +45,7 @@
 
 // Checks magic_enum compiler compatibility.
 #if defined(__clang__) || defined(__GNUC__) && __GNUC__>= 9 || defined(_MSC_VER)
+#  undef MAGIC_ENUM_SUPPORTED
 #  define MAGIC_ENUM_SUPPORTED 1
 #endif
 
