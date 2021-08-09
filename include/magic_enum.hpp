836d0ab2b480fa5d7d65b06b3ff10476f25b0390
neargye <neargye@gmail.com>
2021-08-09 16:24:39 +0300

    clean-up

--- include/magic_enum.hpp
+++ include/magic_enum.hpp
@@ -202,11 +202,10 @@ constexpr string_view pretty_name(string_view name) noexcept {
     if (!((name[i - 1] >= '0' && name[i - 1] <= '9') ||
           (name[i - 1] >= 'a' && name[i - 1] <= 'z') ||
           (name[i - 1] >= 'A' && name[i - 1] <= 'Z') ||
-          (name[i - 1] == '_')
 #if MAGIC_ENUM_OPT_ENABLE_NONASCII
-          || (name[i - 1] & 0x80)
+          (name[i - 1] & 0x80) ||
 #endif
-          )) {
+          (name[i - 1] == '_'))) {
       name.remove_prefix(i);
       break;
     }
@@ -214,11 +213,10 @@ constexpr string_view pretty_name(string_view name) noexcept {
 
   if (name.size() > 0 && ((name.front() >= 'a' && name.front() <= 'z') ||
                           (name.front() >= 'A' && name.front() <= 'Z') ||
-                          (name.front() == '_')
 #if MAGIC_ENUM_OPT_ENABLE_NONASCII
-                          ||(name.front() & 0x80)
+                          (name.front() & 0x80) ||
 #endif
-                          )) {
+                          (name.front() == '_'))) {
     return name;
   }
 
