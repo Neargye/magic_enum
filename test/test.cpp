b0c0e02bdc94d6dac41c1b9a910f3793795dca0e
Daniil Goncharov <neargye@gmail.com>
2022-03-09 10:07:09 +0200

    add compile time enum_index (#159)

--- test/test.cpp
+++ test/test.cpp
@@ -230,12 +230,14 @@ TEST_CASE("enum_index") {
   constexpr auto cr = enum_index(Color::RED);
   Color cg = Color::GREEN;
   REQUIRE(cr.value() == 0);
+  REQUIRE(enum_index<Color::RED>() == 0);
   REQUIRE(enum_index<Color&>(cg).value() == 1);
   REQUIRE(enum_index(cm[2]).value() == 2);
   REQUIRE_FALSE(enum_index(static_cast<Color>(0)).has_value());
 
   constexpr auto no = enum_index(Numbers::one);
   REQUIRE(no.value() == 0);
+  REQUIRE(enum_index<Numbers::one>() == 0);
   REQUIRE(enum_index(Numbers::two).value() == 1);
   REQUIRE(enum_index(Numbers::three).value() == 2);
   REQUIRE_FALSE(enum_index(Numbers::many).has_value());
@@ -243,6 +245,7 @@ TEST_CASE("enum_index") {
 
   constexpr auto dr = enum_index(Directions::Right);
   Directions dl = Directions::Left;
+  REQUIRE(enum_index<Directions::Left>() == 0);
   REQUIRE(enum_index<Directions&>(dl).value() == 0);
   REQUIRE(enum_index<const Directions>(Directions::Down).value() == 1);
   REQUIRE(enum_index(Directions::Up).value() == 2);
@@ -260,6 +263,7 @@ TEST_CASE("enum_index") {
 #endif
 
   constexpr auto nt = enum_index(number::three);
+  REQUIRE(enum_index<number::one>() == 0);
   REQUIRE(enum_index(number::one).value() == 0);
   REQUIRE(enum_index(number::two).value() == 1);
   REQUIRE(nt.value() == 2);
