bd5e9be9bf53d0d200c64cd48da2bd0bf1802466
neargye <neargye@gmail.com>
2022-03-28 18:26:25 +0400

    fix

--- include/magic_enum.hpp
+++ include/magic_enum.hpp
@@ -64,13 +64,13 @@
 #  pragma clang diagnostic push
 #elif defined(__GNUC__)
 #  pragma GCC diagnostic push
+#  pragma GCC diagnostic ignored "-Wmaybe-uninitialized" // May be used uninitialized 'return {};'.
 #elif defined(_MSC_VER)
 #  pragma warning(push)
-#  pragma warning(disable : 4702)
-#  pragma warning(disable : 26819)
-#  pragma warning(disable : 26495)
-#  pragma warning(disable : 28020)
-#  pragma warning(disable : 26451)
+#  pragma warning(disable : 4702) // Unreachable code.
+#  pragma warning(disable : 26495) // Variable 'static_string<N>::chars_' is uninitialized.
+#  pragma warning(disable : 28020) // Arithmetic overflow: Using operator '-' on a 4 byte value and then casting the result to a 8 byte value.
+#  pragma warning(disable : 26451) // The expression '0<=_Param_(1)&&_Param_(1)<=1-1' is not true at this call.
 #endif
 
 // Checks magic_enum compiler compatibility.
