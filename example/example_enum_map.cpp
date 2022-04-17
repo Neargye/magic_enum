cf80d1f5771980c9ae9a17fb9d6d4d249a6d18a6
jlwoolf <jlwoolf@protonmail.com>
2022-04-17 01:10:41 -0600

    added enum_map type

---
+++ example/example_enum_map.cpp
@@ -0,0 +1,148 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <typeinfo>
+
+#include <magic_enum_map.hpp>
+
+enum Color
+{
+    RED = 0,
+    BLUE = 1,
+    GREEN = 2
+};
+
+enum KeyState {
+    KEY_PRESSED,
+    KEY_RELEASED,
+    KEY_UP,
+    KEY_DOWN
+};
+
+struct RGB
+{
+    int r, g, b;
+
+    friend std::ostream & operator<<(std::ostream &os, const RGB &rgb) {
+        os << "{" << rgb.r << ", " << rgb.g << ", " << rgb.b << "}";
+        return os;
+    }
+};
+
+void construct_map() {
+    std::cout << "\nCreate map and then set values:" << std::endl;
+    magic_enum::enum_map<Color, RGB, std::map> default_map;
+    default_map[Color::RED] = RGB{255, 0, 0};
+    default_map[Color::BLUE] = RGB{0, 0, 255};
+    default_map[Color::GREEN] = RGB{0, 255, 0};
+
+    std::cout << "Color::RED is: " << default_map[Color::RED] << std::endl;
+    std::cout << "Color::BLUE is: " << default_map[Color::BLUE] << std::endl;
+    std::cout << "Color::GREEN is: " << default_map[Color::GREEN] << std::endl;
+
+    std::cout << "\nCreate map and pass initial values through constructor:" << std::endl;
+    magic_enum::enum_map<Color, RGB, std::map> const_map(RGB{255, 0, 0}, RGB{0, 0, 255}, RGB{0, 255, 0});
+    std::cout << "Color::RED is: " << const_map[Color::RED] << std::endl;
+    std::cout << "Color::BLUE is: " << const_map[Color::BLUE] << std::endl;
+    std::cout << "Color::GREEN is: " << const_map[Color::GREEN] << std::endl;
+
+    std::cout << "\nCreate map and pass iteratable constructor:" << std::endl;
+    std::vector<RGB> values = {{255, 0, 0}, {0, 0, 255}, {0, 255, 0}};
+    magic_enum::enum_map<Color, RGB, std::map> iterable_map(values);
+    std::cout << "Color::RED is: " << iterable_map[Color::RED] << std::endl;
+    std::cout << "Color::BLUE is: " << iterable_map[Color::BLUE] << std::endl;
+    std::cout << "Color::GREEN is: " << iterable_map[Color::GREEN] << std::endl;
+
+    std::cout << "\nCreate map using initializer list:" << std::endl;
+    magic_enum::enum_map<Color, RGB, std::map> initializer_map = {
+        {Color::RED, {255, 0, 0}},
+        {Color::BLUE, {0, 0, 255}},
+        {Color::GREEN, {0, 255, 0}}};
+    std::cout << "Color::RED is: " << initializer_map[Color::RED] << std::endl;
+    std::cout << "Color::BLUE is: " << initializer_map[Color::BLUE] << std::endl;
+    std::cout << "Color::GREEN is: " << initializer_map[Color::GREEN] << std::endl;
+}
+
+void partial_map() {
+    std::cout << "\nCreate map defining only some defaults" << std::endl;
+    std::cout << "With the constructor:" << std::endl;
+    magic_enum::enum_map<Color, RGB, std::map> partial_const_map(RGB{255, 0, 0}, RGB{0, 0, 255});
+    std::cout << "Color::RED is: " << partial_const_map[Color::RED] << std::endl;
+    std::cout << "Color::BLUE is: " << partial_const_map[Color::BLUE] << std::endl;
+    std::cout << "Color::GREEN is: " << partial_const_map[Color::GREEN] << std::endl;
+
+    std::cout << "\nWith an iterable:" << std::endl;
+    std::vector<RGB> partial_values = {{255, 0, 0}, {0, 0, 255}};
+    magic_enum::enum_map<Color, RGB, std::map> partial_iterable_map(partial_values);
+    std::cout << "Color::RED is: " << partial_iterable_map[Color::RED] << std::endl;
+    std::cout << "Color::BLUE is: " << partial_iterable_map[Color::BLUE] << std::endl;
+    std::cout << "Color::GREEN is: " << partial_iterable_map[Color::GREEN] << std::endl;
+
+    std::cout << "\nAnd with an initializer list:" << std::endl;
+    magic_enum::enum_map<Color, RGB, std::map> partial_initializer_map = {
+        {Color::RED, {255, 0, 0}},
+        {Color::GREEN, {0, 255, 0}}};
+    std::cout << "Color::RED is: " << partial_initializer_map[Color::RED] << std::endl;
+    std::cout << "Color::BLUE is: " << partial_initializer_map[Color::BLUE] << std::endl;
+    std::cout << "Color::GREEN is: " << partial_initializer_map[Color::GREEN] << std::endl;
+}
+
+void iterate_map() {
+    std::cout << "\nIterate through map:" << std::endl;
+    magic_enum::enum_map<Color, RGB, std::map> map = {
+        {Color::RED, {255, 0, 0}},
+        {Color::BLUE, {0, 0, 255}},
+        {Color::GREEN, {0, 255, 0}}};
+
+    for(auto [color, rgb] : map)
+        std::cout << magic_enum::enum_name(color) << " " <<rgb << std::endl;
+}
+
+void insert_map() {
+    std::cout << "\nInsert into map" << std::endl;
+    magic_enum::enum_map<Color, RGB, std::map> map;
+
+    std::cout << "By passing arguments directly:" << std::endl;
+    map.insert(map.begin(), RGB{255, 0, 0}, RGB{0, 0, 255});
+    std::cout << "Color::RED is: " << map[Color::RED] << std::endl;
+    std::cout << "Color::BLUE is: " << map[Color::BLUE] << std::endl;
+    std::cout << "Color::GREEN is: " << map[Color::GREEN] << std::endl;
+
+    std::cout << "\nBy passing an iterable:" << std::endl;
+    std::vector<RGB> values = {{255, 5, 5}, {5, 5, 255}};
+    map.insert(map.begin(), values);
+    std::cout << "Color::RED is: " << map[Color::RED] << std::endl;
+    std::cout << "Color::BLUE is: " << map[Color::BLUE] << std::endl;
+    std::cout << "Color::GREEN is: " << map[Color::GREEN] << std::endl;
+
+    std::cout << "\nOr by passing another map/iterable pair:" << std::endl;
+    std::map<Color, RGB> normal_map = {
+        {Color::RED, {255, 15, 15}},
+        {Color::BLUE, {15, 255, 15}}
+    };
+    map.insert(normal_map);
+    std::cout << "Color::RED is: " << map[Color::RED] << std::endl;
+    std::cout << "Color::BLUE is: " << map[Color::BLUE] << std::endl;
+    std::cout << "Color::GREEN is: " << map[Color::GREEN] << std::endl;
+}
+
+void types_map() {
+    std::cout << "Finally, you can specify the map object used" << std::endl;
+    std::cout << "Just make sure your map is an iterable" << std::endl;
+
+    magic_enum::enum_map<Color, int, std::unordered_map> unordered_map;
+    std::cout << "enum_type: " << typeid(magic_enum::enum_map<Color, int, std::unordered_map>::enum_type).name() << std::endl;
+    std::cout << "mapped_type: " << typeid(magic_enum::enum_map<Color, int, std::unordered_map>::mapped_type).name() << std::endl;
+    std::cout << "value_type: " << typeid(magic_enum::enum_map<Color, int, std::unordered_map>::value_type).name() << std::endl;
+    std::cout << "map_type: " << typeid(magic_enum::enum_map<Color, int, std::unordered_map>::map_type).name() << std::endl;
+}
+
+int main()
+{   
+    construct_map();
+    partial_map();
+    iterate_map();
+    insert_map();
+    types_map();
+}
---
+++ include/magic_enum_map.hpp
@@ -0,0 +1,133 @@
+#ifndef MAGIC_ENUM_MAP_HPP
+#define MAGIC_ENUM_MAP_HPP
+
+#include <map>
+#include <initializer_list>
+
+#include "magic_enum.hpp"
+
+namespace magic_enum
+{
+    using std::begin;
+    using std::end;
+
+    template <typename C>
+    struct is_iterable
+    {
+        typedef long false_type;
+        typedef char true_type;
+
+        template <class T>
+        static false_type check(...);
+        template <class T>
+        static true_type check(int,
+                               typename T::const_iterator = C().end());
+
+        const static bool value = sizeof(check<C>(0)) == sizeof(true_type);
+    };
+
+    template <
+        class E,
+        class T,
+        template <class...> class Map,
+        class... MapArgs>
+    class enum_map
+    {
+        static_assert(std::is_enum_v<E>, "magic_enum::enum_map requires enum type");
+        
+
+    public:
+        typedef Map<E, T, MapArgs...> map_type;
+        typedef typename map_type::iterator iterator;
+        typedef typename map_type::const_iterator const_iterator;
+        typedef typename map_type::value_type value_type;
+        typedef typename map_type::mapped_type mapped_type;
+        typedef typename map_type::key_type enum_type;
+
+        enum_map()
+        {
+            enum_for_each<E>([this](auto e) { (*this)[e]; });
+        };
+
+        template <
+            typename... Args,
+            typename = typename std::enable_if<(true && ... && std::is_convertible_v<Args, T>), void>::type>
+        enum_map(Args &&...args) : enum_map()
+        {
+            insert(begin(), std::array<T, sizeof...(Args)>{{args...}});
+        }
+
+        template <
+            typename... Args,
+            typename = typename std::enable_if<(true && ... && std::is_convertible_v<Args, T>), void>::type>
+        enum_map(const Args &...args) : enum_map()
+        {
+            insert(begin(), std::array<T, sizeof...(Args)>{{args...}});
+        }
+
+        template <class V,
+                  typename = typename std::enable_if<is_iterable<V>::value>::type>
+        enum_map(const V &values) : enum_map()
+        {
+            insert(begin(), values);
+        }
+
+        enum_map(std::initializer_list<value_type> l) : enum_map() {
+            for(const value_type &v: l)
+                _map[v.first] = v.second;
+        }
+
+        const_iterator begin() const { return _map.begin(); }
+        const_iterator end() const { return _map.end(); }
+        iterator begin() { return _map.begin(); }
+        iterator end() { return _map.end(); }
+
+        iterator find(const E& e) { return _map.find(e); }
+        const_iterator find(const E& e) const { return _map.find(e); }
+
+        T &operator[](const E &e) { return _map[e]; }
+        T &operator[](E &&e) { return _map[e]; }
+
+        T &at(const E &e) { return _map.at(e); }
+        const T &at(const E &e) const { return _map.at(e); }
+
+        template <typename... Args,
+                  typename = typename std::enable_if<(true && ... && std::is_convertible_v<Args, T>), void>::type>
+        iterator insert(iterator begin, Args &&...args)
+        {
+            return insert(begin, std::array<T, sizeof...(Args)>{{args...}});
+        }
+
+        template <typename V,
+                  typename = typename std::enable_if<(is_iterable<V>::value && std::is_convertible_v<typename V::iterator::value_type,T>), void>::type>
+        iterator insert(iterator begin, const V &values)
+        {
+            auto v_it = values.begin();
+            while (v_it != values.end() && begin != end())
+            {
+                begin->second = *v_it;
+                begin++;
+                v_it++;
+            }
+
+            return begin;
+        }
+
+        template <typename V,
+                  typename = typename std::enable_if<(is_iterable<V>::value && std::is_convertible_v<typename V::iterator::value_type, value_type>), void>::type>
+        iterator insert(const V &values)
+        {
+            auto v_it = values.begin();
+            for (; v_it != values.end(); v_it++)
+                _map[v_it->first] = v_it->second;       
+                
+            v_it--;
+            return _map.find(v_it->first);
+        }
+
+    private:
+        map_type _map;
+    };
+};
+
+#endif // MAGIC_ENUM_MAP_HPP
