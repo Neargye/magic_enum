f36f3d93deb6d3fa3203529e35ed6768c27c3444
ezralicodes <ezralicodes@users.noreply.github.com>
2026-08-12 22:17:23 +0800

    Install legacy magic_enum.hpp compatibility shim
    Signed-off-by: ezralicodes <ezralicodes@users.noreply.github.com>

---
+++ include/magic_enum.hpp
@@ -0,0 +1,5 @@
+#pragma once
+
+// Compatibility shim for pre-0.9.7 `#include <magic_enum.hpp>` includes.
+// Prefer `#include <magic_enum/magic_enum.hpp>` for new code.
+#include "magic_enum/magic_enum.hpp"
